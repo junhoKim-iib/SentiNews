0f48a6d0ee6b59523320869c17a122302332eb67
deeptoo2 <deeptoo2@naver.com>
2023-03-11 02:22:05 +0900

    fix: database connection

--- news_sentiment/analysis/views.py
+++ news_sentiment/analysis/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import StockNews, MainNews
 # Create your views here.
 def home(request):
+
+    
     main_news = StockNews.objects.all()
     context = {'main_news': main_news}
     return render(request, 'analysis/home.html', context)
--- news_sentiment/news_sentiment/settings.py
+++ news_sentiment/news_sentiment/settings.py
@@ -92,6 +92,8 @@ DB_HOST = get_secret("DB_HOST")
 DB_DATABASE_ACCOUNT = get_secret("DB_DATABASE_ACCOUNT")
 DB_USER = get_secret("DB_USER")
 DB_PASSWORD = get_secret("DB_PASSWORD")
+DB_DATABASE_NEWS = get_secret("DB_DATABASE_NEWS")
+
 
 DATABASES = {
     'default': {
@@ -101,7 +103,18 @@ DATABASES = {
         'PASSWORD': DB_PASSWORD,
         'HOST': DB_HOST,
         'PORT': '5432',
-    }
+    },
+    
+    'news': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_DATABASE_NEWS,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': '5432',
+    },
+
+
 }
 AUTH_USER_MODEL = 'account.User' # 커스텀 모델 사용시 추가 필요
 
