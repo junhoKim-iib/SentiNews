9d71520fdf4ab26931b5f01b817087e8e35d286c
deeptoo2 <deeptoo2@naver.com>
2024-02-15 03:07:00 +0900

    Add log messages while saving news data

--- news_sentiment/analysis/models.py
+++ news_sentiment/analysis/models.py
@@ -44,16 +44,20 @@ class StockSentiment(models.Model):
 
 # get_main_news() 함수에서 반환하는 데이터를 DB에 저장하는 함수
 def insert_main_news(data):
+    print("saving main news in DB")
     objs = [MainNews(subject=item['subject'], date=item['date'], content=item['content'], url=item['url']) for item in data]
     MainNews.objects.bulk_create(objs, ignore_conflicts=True)
+    print("main news saved in DB")
 
 
 
 # get_stock_news() 함수에서 반환하는 데이터를 DB에 저장하는 함수
 
 def insert_stock_news(data):
+    print("saving stock news in DB")
     objs = [StockNews(subject=item['subject'], date=item['date'], content=item['content'], url=item['url'], company=item['company'], summary=item['summary']) for item in data]
     StockNews.objects.bulk_create(objs, ignore_conflicts=True)
+    print("stock news saved in DB")
 
     
     
