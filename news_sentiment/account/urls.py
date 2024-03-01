c4f6ce3bac8429f09031c0bfc0dc89389c5db893
deeptoo2 <deeptoo2@naver.com>
2024-03-02 03:26:20 +0900

    Modify url path correctly.

--- news_sentiment/account/urls.py
+++ news_sentiment/account/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-    path('register', views.register, name='register'),
-    path('login', views.login_view, name='login'),
-    path('mypage', views.mypage, name='mypage'),
-    path('logout', views.logout_view, name='logout'),
-    path('delete_account', views.delete_account, name='delete_account'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('mypage/', views.mypage, name='mypage'),
+    path('logout/', views.logout_view, name='logout'),
+    path('delete_account/', views.delete_account, name='delete_account'),
 ]
 
