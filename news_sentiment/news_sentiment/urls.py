169c9360877f730f236f60e380a815c719ed739b
deeptoo2 <deeptoo2@naver.com>
2024-03-08 02:39:48 +0900

    Add board app url pattern.

--- news_sentiment/news_sentiment/urls.py
+++ news_sentiment/news_sentiment/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('analysis.urls')),
+    path('', include('analysis.urls')), # main page url
     path('account/', include('account.urls')),
+    path('board/', include('board.urls')),
 ]
